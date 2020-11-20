687586cf2ee56dc57518e94f7399f1461b1a91ea
Chen Zheng <chenzheng@Chens-MacBook-Pro.local>
2020-11-20 11:00:07 +0800

    updated decision_tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,9 @@
-class decisiontree_node:
-    def __init__(self, col=-1, condition=None, left_node=None, right_node=None):
+class node:
+    def __init__(self, index=None, condition=None, left_node=None, right_node=None):
+        self.index = index
         self.condition = condition
-        self.left_node = l_node
-        self.right_node = r_node
+        self.left_node = left_node
+        self.right_node = right_node
 
 
 class DecisionTree:
@@ -12,42 +13,49 @@ class DecisionTree:
         self.num_object = 0
         self.num_attribut = 0
 
-    def get_split(sub_set):
+    def recurrent_split(sub_set):
 
         # stop split
         #   all sub' attributes are the same or
         #   all sub' labels are the same
-        if len(sub):
-            return decisiontree_node(), err
+        if 1:
+            err = 0
+            return node(), err
 
         # split
         best_gini = 1
         best_split = None
         best_condition = None
+        index = None
         for i in range(self.num_attribut):
 
-            current_split, current_condition = split_with_index(sub_set, i)
-            current_gini = gini(current_split)
+            current_split, current_condition, current_gini = split_with_index(sub_set, i)
 
             if current_gini < best_gini:
                 best_gini = current_gini
                 best_split = current_split
                 best_condition = current_condition
+                index = i
 
-        a, a_err = self.get_split(best_split[0])
-        b, b_err = self.get_split(best_split[1])
+        a, a_err = self.recurrent_split(best_split[0])
+        b, b_err = self.recurrent_split(best_split[1])
 
-        return decisiontree_node(a, b, best_condition), a_err + b_err
+        return node(index, a, b, best_condition), a_err + b_err
 
-    def split(self, attribute_index, ):
-        # for()
-        return [a, b], condition
+    # find in what condition is the
+    def split_with_index(self, sub_set, attribute_index,):
+        # using dictionary
+        # for(condition)
+        a, b = sub_set#
+        condition = None
+        gini_value = 0
+        return [a, b], condition, gini_value
 
     def build_tree(self, train_set):
         self.num_object = len(self.train_set)
         self.num_attribut = len(train_set[0]) - 1
-        self.root, err_object = get_split(train_set)
-        err_s = err_object / num_object
+        self.root, err_object = self.recurrent_split(train_set)
+        err_s = err_object / self.num_object
         return err_s
 
     def classify(self, input):
