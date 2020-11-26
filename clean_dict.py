463a1702643f14b66c5ccbcdd28bac111a3e65dd
Kiaryw <kiarywong@gmail.com>
2020-11-26 19:16:08 +0800

    add README

--- clean_dict.py
+++ clean_dict.py
@@ -1,31 +1,22 @@
-def clean_data(path, test):
+def clean_data(path):
     data = []
-    # index = 0
+    index = 0
     with open(path, 'r') as f:
         lines = f.readlines()
         for index, line in enumerate(lines):
-            if test and index == 0:
-                continue
             temp = []
             line = line.strip()
             value = line.split(', ')
             for i in range(len(value)):
                 if i == len(value)-2:
                     continue
-                if test:
-                    if i == len(value) - 1:
-                        if value[i] == '<=50K.':
-                            temp.append('0')
-                        else:
-                            temp.append('1')
-                        continue
-                else:
-                    if i == len(value) - 1:
-                        if value[i] == '<=50K':
-                            temp.append('0')
-                        else:
-                            temp.append('1')
-                        continue
+
+                if i == len(value)-1:
+                    if value[i] == '<=50K':
+                        temp.append('0')
+                    else:
+                        temp.append('1')
+                    continue
 
                 if i == len(value) - 3 or i == len(value) - 4 or i == len(value) - 5 or i == len(
                         value) - 11 or i == len(value) - 13 or i == len(value) - 15:
@@ -34,9 +25,9 @@ def clean_data(path, test):
                 temp.append(value[i])
             if '?' not in temp and index != len(lines)-1:
                 data.append(temp)
-            # index += 1
-            # if index >= 10000:
-            #     break
+            index += 1
+            if index >= 3000:
+                break
 
     f.close()
 
@@ -45,15 +36,15 @@ def clean_data(path, test):
 
 # train_path = 'data/adult.data'
 # test_path = 'data/adult.test'
-# train = clean_data(train_path, False)
-# test = clean_data(test_path, True)
+# train = clean_data(train_path)
+# test = clean_data(test_path)
 #
 # col_labels = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status',
 #               'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss',
 #               'hours_per_week', 'wage_class']
 #
 #
-# for line in test:
+# for line in train:
 #     combine_label = zip(col_labels, line)
 #     print(dict(combine_label))
 # print(len(train))
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from DecisionTree import *
 from clean_dict import *
 
-DT = DecisionTree(20)
+DT = DecisionTree()
 
 # data = [[1,'a', -1],[2,'a',1],[3,'a',1],[4,'a',1],[5,'a',-1],
 #         [1,'b',1],[2,'b',1],[3,'b',1],[4,'b',1],[5,'b',-1],
@@ -17,18 +17,20 @@ DT = DecisionTree(20)
 train_path = 'data/adult.data'
 test_path = 'data/adult.test'
 
-train = clean_data(train_path, False)
-test = clean_data(test_path, True)
-print(DT.build_tree(train))
+train = clean_data(train_path)
+test = clean_data(test_path)
 
-correct = 0
-num_data = 0
-for data in test:
-    num_data += 1
-    predict_label = DT.classify(data)
-    if predict_label == data[-1]:
-        correct += 1
-print(float(correct) / num_data)
-print(correct)
-print(num_data)
-DT.print_tree()
+# print(DT.build_tree(train))
+#
+# correct = 0
+# num_data = 0
+# for data in test:
+#     num_data += 1
+#     predict_label = DT.classify(data)
+#     if predict_label == test[-1]:
+#         correct += 1
+# print(float(correct) / num_data)
+# DT.print_tree()
+
+for i in train:
+    print(i[-1])
